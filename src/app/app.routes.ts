bde755e0d50aa252e920045e4cf57e925b09710f
marchean <anais.marchetti@etu.univ-grenoble-alpes.fr>
2025-01-13 15:56:14 +0100

    update: responsive; add: links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {MarkbookComponent} from "./compos/markbook/markbook.component";
+import {AccueilComponent} from "./compos/accueil/accueil.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+    { path: 'accueil', component: AccueilComponent },
+];
--- src/app/compos/markbook/markbook.component.ts
+++ src/app/compos/markbook/markbook.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-markbook',
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './markbook.component.html',
   styleUrl: './markbook.component.scss'
 })
