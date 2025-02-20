ad382822d938a67bb843f7b4b5d3b11e806cc2fb
marchean <anais.marchetti@etu.univ-grenoble-alpes.fr>
2025-02-20 17:17:28 +0100

    update: disabled contact until complete. add: github link markbook

--- src/app/compos/contact/contact.component.ts
+++ src/app/compos/contact/contact.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-contact',
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink,
+    FormsModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  message: string = '';
+  mail: string = '';
 
+  regex = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]+$');
+
+  isValid(){
+    return !(this.message && this.regex.test(this.mail));
+  }
 }
